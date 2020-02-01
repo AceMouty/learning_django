f73e76718a203bdb749185548490f3ce6f06ccdf
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-01 16:48:37 -0600

    Created a lead class and loaded into the db

---
+++ react_django/leads/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+# this creates a table with the needed columns to be added to the DB
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- react_django/react_django/settings.py
+++ react_django/react_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add in apps
+    'leads',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
